f1e5af726d90f497751935d96106d3cc6bfe330c
amLiux <marceliux@jardinbinario.com>
2023-03-12 12:58:35 -0600

    fixed the CGPT prompt as it was not working for some languages

--- openai_instance.py
+++ openai_instance.py
@@ -8,7 +8,7 @@ class OpenAIProvider:
         self.create_chat = openai.ChatCompletion.create
         openai.api_key = self.api_key
     def get_formatted_question(self, user_role, description, language):
-        return "Hello ChatGPT, I'm a {user_role} working on a ticket that says: {description}, would you mind on giving me some guidance like links, code examples and such, please reply on markdown format but don't use headings, please reply back in: {language}".replace('{user_role}', user_role).replace('{description}', description).replace('{language}', language)
+        return "Hello ChatGPT, I'm a {user_role} working on a ticket that says: {description}, would you mind on giving me some guidance like links, code examples and such, please reply on markdown format but don't use headings, please reply back in the following language: {language} ".replace('{user_role}', user_role).replace('{description}', description).replace('{language}', language)
     def ask_chatgpt(self, message):
         r = self.create_chat( 
             model="gpt-3.5-turbo",
